b0add0969727afc77f510f69535a9e10dbcac6d5
Vishesh Bangotra <visheshbangotra@gmail.com>
2017-02-28 22:13:23 +0530

    project init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aetos_serialiser',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/FerrumAquila/aetos_serialiser',
+      author='ironeagle',
+      author_email='visheshbangotra@gmail.com',
+      license='MIT',
+      packages=['aetos_serialiser'],
+      zip_safe=False)
