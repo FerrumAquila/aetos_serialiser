52cada88a3546b134b990f753cd589366021a65f
Vishesh Bangotra <visheshbangotra@gmail.com>
2017-02-28 22:25:21 +0530

    changed description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='aetos_serialiser',
-      version='0.1',
-      description='The funniest joke in the world',
+      version='1.0.1',
+      description='Simple Serialiser for Simple Developers',
       url='https://github.com/FerrumAquila/aetos_serialiser',
       author='ironeagle',
       author_email='visheshbangotra@gmail.com',
